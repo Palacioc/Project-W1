26d4a0ae51fe2d4b371df76cd55bbc6d05272ebc
Carlos de Palacio <carlosdepalacio@outlook.com>
2017-02-03 11:47:42 +0100

    V1

--- js/app.js
+++ js/app.js
@@ -1,29 +1,261 @@
 // $(document).ready(function() {
-  console.log("app linked");
-  var round = new Round();
-  console.log(round);
-  round.setTarget();
-  console.log("we set the target: " + round.target);
-  round.setNumbersToPlayWith();
-  console.log("we set the numbers to play with: " + round.numbersToPlayWith);
+
+//Cosas por hacer manana
+// poner musiquita
+// refactorizar
+// poder hacer click en las cartas
+// hacer responsive
+
+  function appendNewEntry(){
+    var entry = document.createElement("div");
+    entry.className = "card entry";
+    var marker = document.getElementsByClassName("marker")[0].cloneNode(true);
+    marker.childNodes.forEach(function(elem){elem.id = null;});
+    entry.appendChild(marker);
+    var player = document.getElementsByClassName("player-container")[0];
+    player.insertAdjacentHTML('afterEnd', entry.outerHTML);
+  }
+
+  function successfulSendAnimate(){
+    var eq = document.getElementById("equation");
+    var send = document.getElementById("check");
+    var feedback = document.getElementById("feedback");
+    feedback.style.transform = "scale(1)";
+    eq.style.transform = "translate(-70px, 0px)";
+    send.style.transform = "translate(75px, 0px)";
+    setTimeout(function(){
+      feedback.style.transform = "scale(.7)";
+      eq.style.transform = "translate(0px, 0px)";
+      send.style.transform = "translate(0px, 0px)";
+    }, 600);
+  }
+
+  function updateFeedback(message){
+    var feedback = document.getElementById("feedback");
+    switch (message) {
+      case "NVWN":
+        feedback.innerHTML = "Wrong numbers!";
+        feedback.className = "bad-feedback";
+        break;
+      case "NVWE":
+        feedback.innerHTML = "Invalid equation!";
+        feedback.className = "bad-feedback";
+        break;
+      case "NVWD":
+        feedback.innerHTML = "Invalid division!";
+        feedback.className = "bad-feedback";
+        break;
+      case "NVLE":
+        feedback.innerHTML = "Letters?";
+        feedback.className = "bad-feedback";
+        break;
+      case "VNHS":
+        feedback.innerHTML = "New highscore!";
+        feedback.className = "good-feedback";
+        break;
+      case "VMEH":
+        feedback.innerHTML = "Meh...";
+        feedback.className = "meh-feedback";
+        break;
+      default:
+    }
+  }
+
+  function updateMarker(){
+    var marker = document.getElementsByClassName("marker")[0];
+    marker.style.display = "";
+    var playerName = document.getElementById("player-name");
+    playerName.innerHTML = round.players[round.activePlayer].name;
+    var playerScore = document.getElementById("score");
+    playerScore.innerHTML = "(Score: " + ((1000-round.players[round.activePlayer].highScore)/100) + "/10)";
+    var bestApprox = document.getElementById("best-approx");
+    bestApprox.innerHTML = "Best approximation: " + round.players[round.activePlayer].highScoreResult;
+  }
+
+  function newEntry(){
+    var player = document.getElementsByClassName("player-container")[0];
+    player.insertAdjacentHTML('afterend','<div class="card player-container"><div class="marker"><span  class="stats player-name">Player name</span><span  class="stats score">Score: </span><span  class="stats">Best approximation: </span></div></div>)');
+  }
+
+  function  clearEquationField (){
+    document.getElementById("equation").value = "Type here";
+  }
+
+  function  clearTimer (){
+    document.getElementById("timer-bar").style.width = "0px";
+  }
+
+  function clearPlayer (){
+    clearEquationField();
+    clearTimer();
+  }
+
+  function updateWelcome(){
+    var name = document.getElementsByClassName("welcome")[0];
+    name.innerHTML = ("Hi, " + round.players[round.activePlayer].name + "! When you are ready:");
+  }
+
+  function displayNumbersToPlayWith(){
+    round.numbersToPlayWith.forEach(function(elem, index){
+      var card = document.getElementById("number-" + (index+1));
+      card.innerHTML = elem;
+    });
+  }
+
+  function hideNumbersToPlayWith(){
+      for (var i = 1; i < 7; i++) {
+      var card = document.getElementById("number-" + i);
+      card.innerHTML = "";
+      }
+  }
+
+  function displayTarget(){
+    var card = document.getElementById("target");
+    card.innerHTML = round.target;
+  }
+
+  function hideTarget(){
+    var card = document.getElementById("target");
+    card.innerHTML = "";
+  }
+
+  var container = document.getElementById("container");
+  var startButton = document.getElementById("start");
+  var fadeBackground = document.getElementsByClassName("fade-background")[0];
+  var addButton = document.getElementById("add");
+  var removeButton = document.getElementById("remove");
+  var numberOfPlayers = 1;
+
+  function hideWelcome(){
+    var container = document.getElementsByClassName("cover")[0];
+    container.style.display = "none";
+  }
+  function showWelcome(){
+    var container = document.getElementsByClassName("cover")[0];
+    container.style.display = "";
+  }
+
+  function showResults(){
+    var container = document.getElementsByClassName("cover")[1];
+    container.className = "cover results";
+  }
+
+  function hideResults(){
+    var container = document.getElementsByClassName("cover")[1];
+    console.dir(container);
+    container.className = "cover results hidden";
+  }
+
+  function updateResults(){
+    var container = document.getElementsByClassName("cover")[1];
+    var string1 = "";
+    var string2 = "";
+    if (round.winner.length === 1) {
+      string1 = "<br>The winner is ";
+    }else {
+      string1 = "<br>The winners are ";
+    }
+    round.winner.forEach(function(elem, index, array){
+      if (index === 0) {
+        string2 += elem.name;
+      }else if (index < array.length - 1) {
+        string2 += ", " + elem.name;
+      }else{
+        string2 += " and " + elem.name;
+      }
+    });
+
+    container.innerHTML = string1 + string2;
+  }
+
+  function updateIntermediateResult(whyEnd){
+    var container = document.getElementsByClassName("cover")[1];
+    var string = "";
+    switch (whyEnd) {
+      case "win":
+        string = "<br>You reached the exact number!<br>Congratulations!";
+        break;
+      case "timeOut":
+        string = "<br>You ran out of time!<br>Your best approximation is: " + round.players[round.activePlayer].highScoreResult;
+        break;
+      default:
+      }
+    container.innerHTML = string;
+  }
+
+  function addPlayerBox () {
+    if (numberOfPlayers<6) {
+      var lastPlayer = document.getElementsByClassName("namesInput")[numberOfPlayers-1];
+      lastPlayer.insertAdjacentHTML("afterend",'<input type="text" name="" value="Player ' + (numberOfPlayers+1) + ' name" class="namesInput">');
+      numberOfPlayers++;
+    }
+  }
+
+  removeButton.onclick = function(){
+    if (numberOfPlayers>1) {
+      var namesCard = document.getElementById("names-card");
+      var lastPlayer = document.getElementsByClassName("namesInput")[numberOfPlayers-1];
+      namesCard.removeChild(lastPlayer);
+      numberOfPlayers--;
+    }
+  };
+
+
+  var round;
+  startButton.onclick = triggerGame;
+
+  function triggerGame () {
+    //Create new round and initialize values:
+    round = new Round();
+    round.setTarget();
+    round.setNumbersToPlayWith();
+    //Store players in round:
+    var finalNames = document.getElementsByClassName("namesInput");
+    round.storePlayers(finalNames);
+    //Show game board
+    fadeBackground.style.display = "none";
+    round.activePlayer = 0;
+    updateWelcome();
+    showWelcome();
+  }
 
   var equation = document.getElementById('equation');
   var btn = document.getElementById("check");
+
+  equation.onkeydown = function(e){if(e.keyCode == 13){round.send();}};
   btn.onclick = function(){round.send();};
-  //initialize Round
-  //print board
-    //print numbers to play with
-    //print target number
-    //print textbox
+  var sintonia = new Audio('audio/sintonia.mp3');
 
-    //textbox has an event when enter
-    //validate function
-    //if yes calcscorefuncion
-    //updatescorefunction
-    //cleartextinput
+  var startTurn = document.getElementById("start-turn");
+  startTurn.onclick = function(){
+    hideWelcome();
+    displayNumbersToPlayWith();
+    displayTarget();
+    updateMarker();
+    sintonia.currentTime = 0;
+    sintonia.play();
+    var timeBar = document.getElementById("timer-bar");
+    var width = 1;
+    var red = false;
+    round.time = setInterval(frame, 10);
+    function frame(){
+      if (red === false && width>=3500) {
+        red = true;
+        timeBar.style.backgroundColor = "red";
+      }
+      if (width>=4500) {
+        clearInterval(round.time);
+        round.endTurn("timeOut");
+      } else {
+        width++;
+        timeBar.style.width = width/45 + '%';
+      }
+    }
+  };
 
-  //start timer
-  //if timer is out or if score is 0 break
 
-
-// });
+  addButton.onclick = addPlayerBox;
+  var namesInput = document.getElementsByClassName("namesInput")[0];
+  namesInput.onkeydown = function(e){if(e.keyCode === 13){
+    triggerGame();
+  }};
--- js/logic.js
+++ js/logic.js
@@ -3,15 +3,45 @@ function Round () {
   this.numbersChoice = [1,2,3,4,5,6,7,8,9,10,25,50,75,100];
   this.numbersToPlayWith = [0,0,0,0,0,0];
   this.target = 0;
-  this.player1 = new Player();
-  this.player2 = new Player();
+  this.activePlayer = -1;
+  this.players = [];
+  this.winner = -1;
+  this.time = 0;
 }
 
 function Player(){
   this.name = "nombre";
   this.highScore = 999;
+  this.highScoreResult = 0;
   this.numberOfTries = 0;
 }
+
+Round.prototype.setResults = function(){
+  var best = [];
+  this.players.reduce(function(top, elem, index){
+    console.log("probando");
+    if (elem.highScore === top) {
+      console.log("probando igual");
+      best.push(elem);
+      return elem.highScore;
+    }else if (elem.highScore < top) {
+      best = [];
+      best.push(elem);
+      return elem.highScore;
+    }else{return top;}
+  }, 1000);
+  console.log(best);
+  this.winner = best;
+};
+
+Round.prototype.storePlayers = function(names){
+  var players = this.players;
+  names = Array.from(names);
+  names.forEach(function(elem, index, array){
+    players[index] = new Player();
+    players[index].name = elem.value;
+  });
+};
 
 Round.prototype.splitInNumbers = function(string){
   var splitted = string.replace(/\(/g, " ").replace(/\)/g, " ").replace(/\+/g, " ").replace(/\-/g, " ").replace(/\*/g, " ").replace(/\//g, " ").split(" ");
@@ -28,22 +58,32 @@ Round.prototype.setTarget = function(){
   this.target = Math.floor(Math.random() * (999 - 101 + 1)) + 101;
 };
 
-//Para la funcion validate hay tres condiciones:
-//  -cond1: que todos los caracteres sean de los numeros a elegir y que no se utilice uno mismo mas de una vez
-//  -cond2: que las operaciones den como resultado un numero (y no un fallo)
-
 Round.prototype.cond1 = function(numbersUsed, numbersChoice){
   var clone = numbersChoice.slice(0);
   var isok = true;
   numbersUsed.forEach(function(elem){if(clone.indexOf(elem)<0){isok = false; return false;}else{clone[clone.indexOf(elem)]=-1;}});
+  if (isok === false) {
+    updateFeedback('NVWN');
+    successfulSendAnimate();
+  }
   return isok;
 };
 
 Round.prototype.cond2 = function(string){
-  try{eval(string);}catch(e){return false;}
+  var red = false;
+  var conDecimales = 0;
+  try{conDecimales = eval(string);}catch(e){
+    updateFeedback('NVWE');
+    successfulSendAnimate();
+    return false;
+  }
+  if (Math.floor(conDecimales)!==conDecimales){
+    updateFeedback('NVWD');
+    successfulSendAnimate();
+    return false;
+  }
   return true;
 };
-
 
 //Funcion VALIDATE devuelve true si es valido el string dados los numeros disponibles:
 Round.prototype.validate = function (string, numbersChoice) {
@@ -55,22 +95,68 @@ Round.prototype.calculateScore = function(string){
   return Math.abs(this.target - eval(string));
 };
 
-Round.prototype.updateHighScore = function(player, score){
-  if (score < player.highScore) { player.highScore=score; return true;}
+Round.prototype.updateHighScore = function(player, score, scoreResult){
+  if (score < player.highScore) { player.highScore=score; player.highScoreResult = scoreResult;return true;}
   return false;
 };
 
+Round.prototype.isWin = function(player){
+  return player.highScore === 0;
+};
+
+Round.prototype.endTurn = function(whyEnd){
+  var that = round;
+  if (whyEnd === "win") {
+    clearInterval(that.time);
+    sintonia.pause();
+    sintonia.currentTime = 0;
+  }
+  appendNewEntry();
+  updateIntermediateResult(whyEnd);
+  showResults();
+  clearPlayer();
+  that.activePlayer++;
+  setTimeout(hideResults,2000);
+  setTimeout(that.reset,2000);
+};
+
+Round.prototype.reset = function (){
+  var that = round; //No consigo que me funcione de otra forma!! He probado todo..
+  if (that.activePlayer < that.players.length) {
+    updateWelcome();
+    showWelcome();
+    hideNumbersToPlayWith();
+    hideTarget();
+  }else {
+    hideNumbersToPlayWith();
+    hideTarget();
+    that.setResults();
+    updateResults();
+    showResults();
+  }
+};
+
 Round.prototype.send = function(){
-  that = this;
   var latestEq = equation.value;
-  console.log("The value of the text field is: " + eval(latestEq));
-  var valid = that.validate(latestEq, round.numbersToPlayWith);
+  var valid = this.validate(latestEq, round.numbersToPlayWith);
   console.log("Is this equation valid?: " + valid);
   if (valid) {
-    var latestScore = that.calculateScore(latestEq);
+    var latestScoreResult = eval(latestEq);
+    console.log("The value of the text field is: " + latestScoreResult);
+    var latestScore = this.calculateScore(latestEq);
     console.log("Score obtained: " + latestScore);
-    if (that.updateHighScore(that.player1, latestScore)) {
+    if (this.updateHighScore(this.players[this.activePlayer], latestScore, latestScoreResult)) {
+      updateMarker();
       console.log("New high score!");
+      updateFeedback('VNHS');
+      successfulSendAnimate();
+      if (this.isWin(this.players[this.activePlayer])) {
+        clearTimeout(this.time);
+        this.endTurn("win");
+      }
+    }else{
+      updateFeedback('VMEH');
+      successfulSendAnimate();
     }
   }
 };
